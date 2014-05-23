1dc30812e10052570e129b775d1f79e72869e346
larrykooper <larry.kooper@gmail.com>
2014-05-23 16:05:27 -0400

    Added index route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+var db = require('../models')
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
+exports.index = function(req, res){
+  db.Post.findAll().success(function(posts) {
+    res.render('index', {
+      title: 'Express',
+      posts: posts
+    })
+  })
+}
