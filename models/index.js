c8eea9c89eb7ba1310a08343fc931efe39f6f018
larrykooper <larry.kooper@gmail.com>
2014-05-24 00:37:06 -0400

    Code cleanup and use of config file

--- models/index.js
+++ models/index.js
@@ -1,45 +1,47 @@
+var config = require('./config');
+var db_conn;
+
 if (!global.hasOwnProperty('db')) {
-  var Sequelize = require('sequelize')
-    , sequelize = null
+      var Sequelize = require('sequelize')
+       , sequelize = null
+    //console.log(process.env.NODE_ENV);
+    if (process.env.NODE_ENV === 'development') {
+        // the application is executed on the local machine ...
+        db_conn = config.database.development;
+        sequelize = new Sequelize(db_conn.database, db_conn.username, db_conn.password,  {
+            dialect:  db_conn.dialect,
+            protocol: db_conn.protocol,
+            port:     db_conn.port,
+            host:     db_conn.host,
+            logging:  console.log
+        });
+    } else {
+        // the application is executed on Digital Ocean ... use the postgres database
+        db_conn = config.database.production;
+        sequelize = new Sequelize(db_conn.database, db_conn.username, db_conn.password, {
+            dialect:  db_conn.dialect,
+            protocol: db_conn.protocol,
+            port:     db_conn.port,
+            host:     db_conn.host,
+            logging:  console.log
+        });
+     }
  
-  if (process.env.NODE_ENV === 'development') {
-    // the application is executed on Digital Ocean ... use the postgres database
-    var match = process.env.HEROKU_POSTGRESQL_BRONZE_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/)
- 
-    sequelize = new Sequelize('melville', 'root', 'b2J3Pj1g93iT0cAX', {
-      dialect:  'postgres',
-      protocol: 'postgres',
-      port:     '49164',
-      host:     '172.17.42.1',
-      logging:  console.log 
-    })
-  } else {
-    // the application is executed on the local machine ... 
-    sequelize = new Sequelize('melville_dev', 'larry1mbp', 'r3cycl3', {    
-      dialect:  'postgres',
-      protocol: 'postgres',
-      port:     '5432',
-      host:     'localhost',
-      logging:  console.log
-    })
-  }
- 
-  global.db = {
-    Sequelize: Sequelize,
-    sequelize: sequelize,
-    User:      sequelize.import(__dirname + '/user'),
-    Post:   sequelize.import(__dirname + '/post')
- 
-    // add your other models here
-  }
- 
+    global.db = {
+        Sequelize: Sequelize,
+        sequelize: sequelize,
+        User:      sequelize.import(__dirname + '/user'),
+        Post:   sequelize.import(__dirname + '/post')
+       // add your other models here
+    }
+
   /*
     Associations can be defined here. E.g. like this:
     global.db.User.hasMany(global.db.SomethingElse)
   */
-  
+
   //for more on assoc see http://sequelizejs.com/articles/express
-  
+
 }
- 
+
 module.exports = global.db
