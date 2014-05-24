74418b3d5df712ee706d94c1f08861f04cb9264f
larrykooper <larry.kooper@gmail.com>
2014-05-24 10:41:16 -0400

    Changed the environment setup.

--- models/index.js
+++ models/index.js
@@ -1,32 +1,32 @@
-var config = require('../config');
-var db_conn;
-
 if (!global.hasOwnProperty('db')) {
       var Sequelize = require('sequelize')
        , sequelize = null
     //console.log(process.env.NODE_ENV);
     if (process.env.NODE_ENV === 'development') {
-        // the application is executed on the local machine ...
-        db_conn = config.database.development;
-        sequelize = new Sequelize(db_conn.database, db_conn.username, db_conn.password,  {
-            dialect:  db_conn.dialect,
-            protocol: db_conn.protocol,
-            port:     db_conn.port,
-            host:     db_conn.host,
+        // the application is executed on the local machine
+
+        var match = process.env.DATABASE_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/)
+
+        sequelize = new Sequelize(match[5], match[1], match[2], {
+            dialect:  'postgres',
+            protocol: 'postgres',
+            port:     match[4],
+            host:     match[3],
             logging:  console.log
         });
     } else {
-        // the application is executed on Digital Ocean ... use the postgres database
-        db_conn = config.database.production;
-        sequelize = new Sequelize(db_conn.database, db_conn.username, db_conn.password, {
-            dialect:  db_conn.dialect,
-            protocol: db_conn.protocol,
-            port:     db_conn.port,
-            host:     db_conn.host,
+        // the application is executed on Production
+        var match = process.env.DATABASE_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/)
+
+        sequelize = new Sequelize(match[5], match[1], match[2], {
+            dialect:  'postgres',
+            protocol: 'postgres',
+            port:     match[4],
+            host:     match[3],
             logging:  console.log
         });
-     }
- 
+    }
+
     global.db = {
         Sequelize: Sequelize,
         sequelize: sequelize,
