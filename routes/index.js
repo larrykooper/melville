654358d74e6dd60d5fc4d7796fd24a82899021b5
larrykooper <larry.kooper@gmail.com>
2014-05-23 21:14:24 -0400

    Updated title of index page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var db = require('../models')
 exports.index = function(req, res){
   db.Post.findAll().success(function(posts) {
     res.render('index', {
-      title: 'Express',
+      title: 'Melville',
       posts: posts
     })
   })
